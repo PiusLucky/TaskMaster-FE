aa69ec43e388e95fcdaaad036d5bc3ba160ad14f
PiusLucky <luckypius50@gmail.com>
2023-10-01 00:11:22 +0100

    chore: added google font to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,10 +22,8 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+        primary: "#27D17F",
+        danger: "#FF0000",
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
